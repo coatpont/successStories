294562224bf5cc761f88f00fa4ab0ca55a91ed28
Bertrand de Coatpont <lebescon@adobe.com>
2022-10-12 15:45:01 +0200

    Boostrapping...

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,17 +1,6 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
 
 /**
- * collapses all open nav sections
- * @param {Element} sections The container element
- */
-
-function collapseAllNavSections(sections) {
-  sections.querySelectorAll('.nav-sections > ul > li').forEach((section) => {
-    section.setAttribute('aria-expanded', 'false');
-  });
-}
-
-/**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
@@ -28,39 +17,25 @@ export default async function decorate(block) {
 
     // decorate nav DOM
     const nav = document.createElement('nav');
-    nav.innerHTML = html;
-    decorateIcons(nav);
+    nav.setAttribute('class','navbar navbar-expand-lg fixed-top navbar-dark bg-dark');
 
-    const classes = ['brand', 'sections', 'tools'];
-    classes.forEach((e, j) => {
-      const section = nav.children[j];
-      if (section) section.classList.add(`nav-${e}`);
+    // add the fluid-container
+    const container = document.createElement('div');
+    container.setAttribute('class','container-fluid');
+    nav.appendChild(container);
+
+    container.innerHTML = html;
+    container.children[0].setAttribute("class","collapse navbar-collapse");
+    container.children[0].setAttribute("id","navbarSupportedContent");
+    container.children[0].children[0].setAttribute("class","navbar-nav me-auto mb-2 mb-lg-0");
+
+    var children = container.children[0].children[0].childNodes;
+    children.forEach(function(item){
+      try {
+        item.setAttribute('class','nav-item');
+      } catch {}
     });
 
-    const navSections = [...nav.children][1];
-    if (navSections) {
-      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-        navSection.addEventListener('click', () => {
-          const expanded = navSection.getAttribute('aria-expanded') === 'true';
-          collapseAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-        });
-      });
-    }
-
-    // hamburger for mobile
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
-    hamburger.addEventListener('click', () => {
-      const expanded = nav.getAttribute('aria-expanded') === 'true';
-      document.body.style.overflowY = expanded ? '' : 'hidden';
-      nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-    });
-    nav.prepend(hamburger);
-    nav.setAttribute('aria-expanded', 'false');
-    decorateIcons(nav);
     block.append(nav);
   }
 }
