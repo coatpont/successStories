9a9c85a0b8bfe7dd477ddadcd36837cba90a0d01
Bertrand de Coatpont <lebescon@adobe.com>
2022-10-13 10:33:26 +0200

    header styling

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -23,18 +23,36 @@ export default async function decorate(block) {
     const container = document.createElement('div');
     container.setAttribute('class','container-fluid');
     nav.appendChild(container);
-
+    
     container.innerHTML = html;
     container.children[0].setAttribute("class","collapse navbar-collapse");
     container.children[0].setAttribute("id","navbarSupportedContent");
     container.children[0].children[0].setAttribute("class","navbar-nav me-auto mb-2 mb-lg-0");
-
+    
+    // decorate the list
     var children = container.children[0].children[0].childNodes;
     children.forEach(function(item){
       try {
         item.setAttribute('class','nav-item');
+        item.children[0].setAttribute('class','nav-link');
       } catch {}
     });
+
+    // add the hamburger button
+    const hamburger = document.createElement('button');
+    hamburger.setAttribute('type','button');
+    hamburger.setAttribute('class','navbar-toggler');
+    hamburger.setAttribute('data-bs-toggle','collapse');
+    hamburger.setAttribute('data-bs-target','#navbarSupportedContent');
+    hamburger.innerHTML = '<span class="navbar-toggler-icon"></span>';
+    container.insertBefore(hamburger,container.children[0]);
+
+    // add the brand link
+    const brand = document.createElement('a');
+    brand.setAttribute('class','navbar-brand');
+    brand.setAttribute('href','https://www.adobe.com');
+    brand.innerHTML = "AEM Cloud Service";
+    container.insertBefore(brand,container.children[0]);
 
     block.append(nav);
   }
