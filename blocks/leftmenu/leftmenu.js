954117452707f7779d64645e3fb94ddb8f488133
Bertrand de Coatpont <lebescon@adobe.com>
2022-10-19 18:55:58 +0200

    Styling the left menu

--- blocks/leftmenu/leftmenu.js
+++ blocks/leftmenu/leftmenu.js
@@ -1,4 +1,4 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
+import { readBlockConfig } from '../../scripts/scripts.js';
 
 /**
  * loads and decorates the footer
@@ -9,11 +9,28 @@ export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
 
-  const footerPath = cfg.footer || '/leftmenu';
-  const resp = await fetch(`${footerPath}.plain.html`);
-  const html = await resp.text();
-  const footer = document.createElement('div');
-  footer.innerHTML = html;
-  await decorateIcons(footer);
-  block.append(footer);
+  const leftmenuPath = cfg.leftmenu || '/leftmenu';
+  const resp = await fetch(`${leftmenuPath}.plain.html`);
+  if (resp.ok) {
+    const html = await resp.text();
+
+    // decorate nav DOM
+    const list = document.createElement('div');
+    // add the fluid-container
+    list.innerHTML = html;
+
+    // decorate the list
+    const children = list.children[0].children[0].childNodes;
+    children.forEach((item) => {
+      try {
+        item.setAttribute('class', 'nav-item');
+        item.children[0].setAttribute('class', 'list-group-item list-group-item-action');
+        block.append(item.children[0]);
+      } catch {
+        // console.log('error');
+      }
+    });
+
+    block.classList.add('list-group');
+  }
 }
